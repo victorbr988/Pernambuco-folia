1b8774102c34aa8bddab9346c9b6c9ed9152ba22
Victor Gabriel <gabrielvghs666@gmail.com>
2023-02-11 01:58:21 -0300

    first version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Pagination } from "./Routes/Pagination";
 
 function App() {
-  console.log(window.screen.width)
   return (
     <div className="w-full h-screen border-image">
       <Pagination />
